1d4caaf01318f2f5c9e10938090ce16b9cebf67c
clamar92 <cla.mar92@gmail.com>
2024-12-03 17:26:23 +0100

    first commit

--- db.py
+++ db.py
@@ -2,6 +2,6 @@ from pymongo import MongoClient
 import os 
 
 def get_db():
-    mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")  # Default to localhost
+    mongo_uri = os.getenv("MONGO_URI", "mongodb://shared-db:27017/")  # Default to localhost
     client = MongoClient(mongo_uri)
     return client.digital_replica
